a7b56c1b9b6bd546a350cf0a217fd009785d1611
Stuart <stuart.bentley@mindtools.com>
2018-08-07 22:09:59 +0100

    fix format and error

--- src/js/index-ios.js
+++ src/js/index-ios.js
@@ -7,161 +7,104 @@
  *
  */
 
-const inAppPurchase = {utils};
+const inAppPurchase = { utils };
 
-const nativeCall = (name, args = []) =
->
-{
-    return new Promise((resolve, reject) = > {
-        window.cordova.exec((res) = > {
-        resolve(res);
-},
-    (err) =
->
-    {
-        reject(err);
-    }
-,
-    'PaymentsPlugin', name, args
-)
-    ;
-})
-    ;
-}
-;
+const nativeCall = (name, args = []) => {
+    return new Promise((resolve, reject) => {
+        window.cordova.exec((res) => {
+            resolve(res);
+        }, (err) => {
+            reject(err);
+        }, 'PaymentsPlugin', name, args);
+    });
+};
 
-inAppPurchase.getProducts = (productIds) =
->
-{
-    return new Promise((resolve, reject) = > {
-        if(
-    !inAppPurchase.utils.validArrayOfStrings(productIds)
-)
-    {
-        reject(new Error(inAppPurchase.utils.errors[101]));
-    }
-else
-    {
-        return nativeCall('getProducts', [productIds]).then((res) = > {
-            if(
-        !res || !res.products
-    )
-        {
-            resolve([]);
+inAppPurchase.getProducts = (productIds) => {
+    return new Promise((resolve, reject) => {
+        if(!inAppPurchase.utils.validArrayOfStrings(productIds)) {
+            reject(new Error(inAppPurchase.utils.errors[101]));
+        } else {
+            return nativeCall('getProducts', [productIds]).then((res) => {
+                if (!res || !res.products) {
+                    resolve([]);
+                } else {
+                    const arr = res.products.map((val) => {
+                        return {
+                            productId   : val.productId,
+                            title       : val.title,
+                            description : val.description,
+                            priceAsDecimal : val.priceAsDecimal,
+                            price       : val.price,
+                            currency    : val.currency,
+                            introductoryPrice: val.introductoryPrice,
+                            introductoryPriceSupported: val.introductoryPriceSupported
+                        };
+                    });
+                    resolve(arr);
+                }
+            }).catch(reject);
         }
-    else
-        {
-            const arr = res.products.map((val) = > {
+    });
+};
 
-                console.log('products', val);
-            return {
-                productId: val.productId,
-                title: val.title,
-                description: val.description,
-                priceAsDecimal: val.priceAsDecimal,
-                price: val.price,
-                currency: val.currency,
-                introductoryPrice: val.introductoryPrice,
-                introductoryPriceSupported: val.introductoryPriceSupported
-            };
-        })
-            ;
-            resolve(arr);
+inAppPurchase.buy = (productId) => {
+    return new Promise((resolve, reject) => {
+        if(!inAppPurchase.utils.validString(productId)) {
+            reject(new Error(inAppPurchase.utils.errors[102]));
+        } else {
+            nativeCall('buy', [productId]).then((res) => {
+                resolve({
+                    transactionId : res.transactionId,
+                    receipt       : res.receipt,
+                });
+            }).catch(reject);
         }
-    }).
-        catch(reject);
-    }
-})
-    ;
-}
-;
-
-inAppPurchase.buy = (productId) =
->
-{
-    return new Promise((resolve, reject) = > {
-        if(
-    !inAppPurchase.utils.validString(productId)
-)
-    {
-        reject(new Error(inAppPurchase.utils.errors[102]));
-    }
-else
-    {
-        nativeCall('buy', [productId]).then((res) = > {
-            resolve({
-                        transactionId: res.transactionId,
-            receipt
-    :
-        res.receipt,
-    })
-        ;
-    }).
-        catch(reject);
-    }
-})
-    ;
-}
-;
+    });
+};
 
 /**
  * This function exists so that the iOS plugin API will be compatible with that of Android -
  * where this function is required.
  * See README for more details.
  */
-inAppPurchase.subscribe = (productId) =
->
-{
+inAppPurchase.subscribe = (productId) => {
     return inAppPurchase.buy(productId);
-}
-;
+};
 
 /**
  * This function exists so that the iOS plugin API will be compatible with that of Android -
  * where this function is required.
  * See README for more details.
  */
-inAppPurchase.consume = () =
->
-{
+inAppPurchase.consume = () => {
     return Promise.resolve();
-}
-;
+};
 
-inAppPurchase.getReceipt = () =
->
-{
-    return nativeCall('getReceipt').then((res) = > {
+inAppPurchase.getReceipt = () => {
+    return nativeCall('getReceipt').then((res) => {
         let receipt = '';
-    if (res && res.receipt) {
-        receipt = res.receipt;
-    }
-    return receipt;
-})
-    ;
-}
-;
+        if (res && res.receipt) {
+            receipt = res.receipt;
+        }
+        return receipt;
+    });
+};
 
-inAppPurchase.restorePurchases = () =
->
-{
-    return nativeCall('restorePurchases').then((res) = > {
+inAppPurchase.restorePurchases = () => {
+    return nativeCall('restorePurchases').then((res) => {
         let arr = [];
-    if (res && res.transactions) {
-        arr = res.transactions.map((val) = > {
-            return {
-                productId: val.productId,
-                date: val.date,
-                transactionId: val.transactionId,
-                state: val.transactionState,
-            };
-    })
-        ;
-    }
-    return arr;
-})
-    ;
-}
-;
+        if (res && res.transactions) {
+            arr = res.transactions.map((val) => {
+                return {
+                    productId     : val.productId,
+                    date          : val.date,
+                    transactionId : val.transactionId,
+                    state         : val.transactionState,
+                };
+            });
+        }
+        return arr;
+    });
+};
 
 module.exports = inAppPurchase;
